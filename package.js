d9a20b55722771f9171a50199c97ac69fc9317f0
buschevapoly-del <buscheva.poly@gmail.com>
2025-12-29 00:14:22 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "notes-ai-processor",
+  "version": "1.0.0",
+  "description": "Функция для обработки заметок и ИИ-анализа",
+  "main": "index.js",
+  "dependencies": {
+    "ydb": "^0.20.0"
+  },
+  "engines": {
+    "node": "16"
+  }
+}
